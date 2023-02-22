0f0cb57b2f721dc87d3ec8643fc7bfbe9af0ea67
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-22 09:27:06 +0100

    update scripts

--- src/train_network.py
+++ src/train_network.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 # 3D Cellpose Extension.
 # Copyright (C) 2021 D. Eschweiler, J. Stegmaier
@@ -6,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the Liceense at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,16 +18,15 @@
 # as well as for installation instructions.
 """
 
+import glob
+import os
 from argparse import ArgumentParser
 
 import numpy as np
 import torch
-import glob
-import os
-
-from pytorch_lightning import Trainer
-from pytorch_lightning.logging import TestTubeLogger
-from pytorch_lightning.callbacks import ModelCheckpoint
+from lightning.pytorch import Trainer
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import CSVLogger
 
 SEED = 1337
 torch.manual_seed(SEED)
@@ -36,7 +34,6 @@ np.random.seed(SEED)
 
 
 def main(hparams):
-
     """
     Main training routine specific for this project
     :param hparams:
@@ -47,46 +44,45 @@ def main(hparams):
     # ------------------------
     model = network(hparams=hparams)
     os.makedirs(hparams.output_path, exist_ok=True)
-    
+
     # Load pretrained weights if available
-    if not hparams.pretrained is None:
+    if hparams.pretrained is not None:
         model.load_pretrained(hparams.pretrained)
 
-    # Resume from checkpoint if available    
+    # Resume from checkpoint if available
     resume_ckpt = None
     if hparams.resume:
-        checkpoints = glob.glob(os.path.join(hparams.output_path,'*.ckpt'))
+        checkpoints = glob.glob(os.path.join(hparams.output_path, "*.ckpt"))
         checkpoints.sort(key=os.path.getmtime)
-        if len(checkpoints)>0:
+        if len(checkpoints) > 0:
             resume_ckpt = checkpoints[-1]
-            print('Resuming from checkpoint: {0}'.format(resume_ckpt))
-            
+            print(f"Resuming from checkpoint: {resume_ckpt}")
+
     # Set the augmentations if available
-    if not hparams.augmentations is None:
+    if hparams.augmentations is not None:
         model.set_augmentations(hparams.augmentations)
-        
+
     # Save a few samples for sanity checks
-    print('Saving 20 data samples for sanity checks...')
-    model.train_dataloader().dataset.test(os.path.join(hparams.output_path, 'samples'), num_files=20)
+    print("Saving 20 data samples for sanity checks...")
+    model.train_dataloader().dataset.test(
+        os.path.join(hparams.output_path, "samples"), num_files=20
+    )
 
     # ------------------------
     # 2 INIT TRAINER
     # ------------------------
-    
+
     checkpoint_callback = ModelCheckpoint(
         filepath=hparams.output_path,
         save_top_k=1,
-        monitor='epoch',
-        mode='max',
+        monitor="epoch",
+        mode="max",
         verbose=True,
-        period=5
+        period=5,
     )
-    
-    logger = TestTubeLogger(
-        save_dir=hparams.log_path,
-        name='lightning_logs_'+hparams.model.lower()
-    )
-    
+
+    logger = CSVLogger(hparams.log_path)
+
     trainer = Trainer(
         logger=logger,
         checkpoint_callback=checkpoint_callback,
@@ -96,7 +92,7 @@ def main(hparams):
         min_nb_epochs=hparams.epochs,
         max_nb_epochs=hparams.epochs,
         early_stop_callback=False,
-        resume_from_checkpoint=resume_ckpt
+        resume_from_checkpoint=resume_ckpt,
     )
 
     # ------------------------
@@ -105,8 +101,7 @@ def main(hparams):
     trainer.fit(model)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # ------------------------
     # TRAINING ARGUMENTS
     # ------------------------
@@ -116,78 +111,72 @@ if __name__ == '__main__':
 
     # gpu args
     parent_parser.add_argument(
-        '--output_path',
+        "--output_path",
         type=str,
-        default=r'D:\LfB\pytorchRepo\results\Cellpose3D_experiment',
-        help='output path for test results'
+        default=r"D:\LfB\pytorchRepo\results\Cellpose3D_experiment",
+        help="output path for test results",
     )
-    
+
     parent_parser.add_argument(
-        '--log_path',
+        "--log_path",
         type=str,
-        default=r'D:\LfB\pytorchRepo\logs',
-        help='output path for test results'
+        default=r"D:\LfB\pytorchRepo\logs",
+        help="output path for test results",
     )
-    
+
     parent_parser.add_argument(
-        '--distributed_backend',
+        "--distributed_backend",
         type=str,
-        default='dp',
-        help='supports three options dp, ddp, ddp2'
+        default="dp",
+        help="supports three options dp, ddp, ddp2",
     )
-    
+
     parent_parser.add_argument(
-        '--gpus',
-        type=int,
-        default=1,
-        help='number of GPUs to use'
+        "--gpus", type=int, default=1, help="number of GPUs to use"
     )
-    
+
     parent_parser.add_argument(
-        '--no_resume',
-        dest='resume',
-        action='store_false',
+        "--no_resume",
+        dest="resume",
+        action="store_false",
         default=True,
-        help='resume training from latest checkpoint'
+        help="resume training from latest checkpoint",
     )
-    
+
     parent_parser.add_argument(
-        '--pretrained',
+        "--pretrained",
         type=str,
         default=None,
-        nargs='+',
-        help='path to pretrained model weights'
+        nargs="+",
+        help="path to pretrained model weights",
     )
-    
+
     parent_parser.add_argument(
-        '--augmentations',
+        "--augmentations",
         type=str,
         default=None,
-        help='path to augmentation dict file'
+        help="path to augmentation dict file",
     )
-    
+
     parent_parser.add_argument(
-        '--epochs',
-        type=int,
-        default=1000,
-        help='number of epochs'
+        "--epochs", type=int, default=1000, help="number of epochs"
     )
-    
+
     parent_parser.add_argument(
-        '--model',
+        "--model",
         type=str,
-        default='Cellpose3D',
-        help='which model to load (Cellpose3D)'
+        default="Cellpose3D",
+        help="which model to load (Cellpose3D)",
     )
-    
+
     parent_args = parent_parser.parse_known_args()[0]
-    
+
     # load the desired network architecture
-    if parent_args.model.lower() == 'cellpose3d':        
+    if parent_args.model.lower() == "cellpose3d":
         from models.UNet3D_cellpose import UNet3D_cellpose as network
     else:
-        raise ValueError('Model {0} unknown.'.format(parent_args.model))
-    
+        raise ValueError(f"Model {parent_args.model} unknown.")
+
     # each LightningModule defines arguments relevant to it
     parser = network.add_model_specific_args(parent_parser)
     hyperparams = parser.parse_args()
