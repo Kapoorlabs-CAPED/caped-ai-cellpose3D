c7dfd9f46b476cf8e53a3a9a6f4cf318e6b9c6bd
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-22 09:57:12 +0100

    update scripts

--- src/models/UNet3D_cellpose.py
+++ src/models/UNet3D_cellpose.py
@@ -22,8 +22,8 @@ import json
 from argparse import ArgumentParser, Namespace
 from collections import OrderedDict
 
+import lightning.pytorch as pl
 import numpy as np
-import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import torchvision
