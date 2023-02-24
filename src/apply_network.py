335be9b062690d18ebce085880e737e299420d63
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-24 15:34:01 +0100

    cellpose predict 3D

--- src/apply_network.py
+++ src/apply_network.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
     parent_parser.add_argument(
         "--overlap",
         type=int,
-        default=(4, 16, 16),
+        default=(2, 8, 8),
         help="overlap of adjacent patches",
         nargs="+",
     )
@@ -320,7 +320,7 @@ if __name__ == "__main__":
     parent_parser.add_argument(
         "--crop",
         type=int,
-        default=(4, 32, 32),
+        default=(2, 16, 16),
         help="safety crop of patches",
         nargs="+",
     )
